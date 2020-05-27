b48df1268530bbe4feda6047266d1c1658169ef5
Fabio Muller <=>
2020-05-27 11:47:17 -0300

    Snake movements created

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,48 @@ snake[0] = {
   y: 8 * box
 }
 
+let direction = "right"
+
 function criarBG() {
   context.fillStyle = "lightgreen"
   context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
 function criarCobrinha() {
-  for(let i = 0; i < snake.length; i++) {
+  for (let i = 0; i < snake.length; i++) {
     context.fillStyle = "green"
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
 }
 
-criarBG()
-criarCobrinha()
+function iniciarJogo() {
+  criarBG()
+  criarCobrinha()
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  switch (direction) {
+    case "right":
+      snakeX += box
+      break
+    case "left":
+      snakeX -= box
+      break
+    case "up":
+      snakeY -= box
+      break
+    case "down":
+      snakeY += box
+      break
+  }
+
+  snake.pop()
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+  snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
